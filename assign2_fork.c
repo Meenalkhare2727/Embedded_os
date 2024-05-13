c71f352b7285c848a9cc7e60184cfa43a0743f2e
Meenalkhare2727 <meenalkhare027@gmail.com>
2024-05-13 18:24:30 +0530

    Assignments_fork

---
+++ assign2_fork.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/wait.h>
+
+int main(){
+  int ret,s,i,j;
+  i=1;
+  while(1){
+    ret = fork();
+	if(ret == 0){
+      for(i=1;i<=10;i++)
+	  printf("child1 : %d\n",i);
+	  sleep(5);
+	  _exit(0);
+
+	}
+	else if(ret == -1) 
+	  break;
+	else {
+     i++;
+	 printf("child count: %d\n",i);
+	}  
+  }
+  for(j=1; j<=i;j++)
+     wait(&s);
+}
