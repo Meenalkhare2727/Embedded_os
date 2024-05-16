e0aeb40001fac57a5862ddb30142035879e15d33
Meenalkhare2727 <meenalkhare027@gmail.com>
2024-05-16 19:01:58 +0530

    in.txt

---
+++ shell programming/demo39_redir.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<sys/wait.h>
+
+int main(){
+
+          int ret,err,s,fd;
+		  printf("parent started!\n");
+		  ret = fork();
+		  if(ret == 0){
+
+                   fd = open("in.txt", O_RDONLY);
+				   close(0);
+				   dup(fd);
+				   close(fd);
+
+				  
+
+				   err = execlp("ls","ls",NULL);
+				   if(err < 0){
+
+                           perror("exec() failed");
+						   _exit(1);
+
+				   }
+
+	}			   
+				   else
+
+				        waitpid(-1,&s,0);
+						printf("parent completed!\n");
+						return 0;
+ }
